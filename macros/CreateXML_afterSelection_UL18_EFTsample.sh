da17c93d34339131d7be36c1d48affb140116227
Beren Ozek <bozek3@uic.edu>
2024-02-19 22:40:15 +0100

    created bash scripts to create input xml files for EFT samples

--- macros/CreateXML_afterSelection_UL18_EFTsample.sh
+++ macros/CreateXML_afterSelection_UL18_EFTsample.sh
@@ -2,11 +2,11 @@
 #where UHH2 code installed
 pathGL_code=/nfs/dust/cms/user/beozek/uuh2-106X_v2/CMSSW_10_6_28/src/UHH2/
 #where (NOT MERGED) trees after preselection stored
-path_data=/nfs/dust/cms/user/beozek/uuh2-106X_v2/CMSSW_10_6_28/src/UHH2/ZprimeSemiLeptonic/output_DNN/UL18/Preselection/workdir_EFT_preselection/uhh2.AnalysisModuleRunner.MC.
+path_data=/nfs/dust/cms/user/beozek/uuh2-106X_v2/CMSSW_10_6_28/src/UHH2/ZprimeSemiLeptonic/output_DNN/UL18/Preselection/workdir_EFT_preselection_morerootfile/
 #path_data=/nfs/dust/cms/group/zprime-uhh/Analysis_UL18/electron/workdir_Zprime_Analysis_UL18_electron/uhh2.AnalysisModuleRunner.
 
-mkdir $pathGL_code/ZprimeSemiLeptonic/data/Skimming_datasets_EFT_afterpreselection
-cd $pathGL_code/ZprimeSemiLeptonic/data/Skimming_datasets_EFT_afterpreselection
+mkdir $pathGL_code/ZprimeSemiLeptonic/data/Skimming_datasets_EFT_afterpreselection_morerootfiles
+cd $pathGL_code/ZprimeSemiLeptonic/data/Skimming_datasets_EFT_afterpreselection_morerootfiles
 #mkdir $pathGL_code/ZprimeSemiLeptonic/data/Skimming_datasets_UL18_electron
 #cd $pathGL_code/ZprimeSemiLeptonic/data/Skimming_datasets_UL18_electron
 
@@ -18,8 +18,8 @@ for sample_name in EFT_sample
 do
     echo $sample_name
 
-       $pathGL_code/scripts/create-dataset-xmlfile ${path_data}${sample_name}"*.root" MC_$sample_name.xml
-       python $pathGL_code/scripts/crab/readaMCatNloEntries.py 10 MC_$sample_name.xml True
+       $pathGL_code/scripts/create-dataset-xmlfile "/nfs/dust/cms/user/beozek/uuh2-106X_v2/CMSSW_10_6_28/src/UHH2/ZprimeSemiLeptonic/output_DNN/UL18/Preselection/workdir_EFT_preselection_morerootfile/split_1.root" MCv2_$sample_name.xml
+       python $pathGL_code/scripts/crab/readaMCatNloEntries.py 10 MCv2_$sample_name.xml True
 done
 
 
--- macros/CreateXML_for_Analysis_afterPreselection_UL18_EFT.sh
+++ macros/CreateXML_for_Analysis_afterPreselection_UL18_EFT.sh
@@ -5,15 +5,15 @@ pathGL_code=/nfs/dust/cms/user/beozek/uuh2-106X_v2/CMSSW_10_6_28/src/UHH2/
 #where (NOT MERGED) trees after preselection stored
 path_data=/nfs/dust/cms/user/beozek/uuh2-106X_v2/CMSSW_10_6_28/src/UHH2/EFT_output/combined/
 
-mkdir $pathGL_code/ZprimeSemiLeptonic/data/Skimming_datasets_UL18_EFT_samples
-cd $pathGL_code/ZprimeSemiLeptonic/data/Skimming_datasets_UL18_EFT_samples
+mkdir $pathGL_code/ZprimeSemiLeptonic/data/Skimming_datasets_UL18_EFT_samples_morefiles
+cd $pathGL_code/ZprimeSemiLeptonic/data/Skimming_datasets_UL18_EFT_samples_morefiles
 
 # MC
 for sample_name in EFT_sample
 do
     echo $sample_name
 
-       $pathGL_code/scripts/create-dataset-xmlfile ${path_data}"*.root" $sample_name.xml
+       $pathGL_code/scripts/create-dataset-xmlfile ${path_data}"EFT*.root" $sample_name.xml
        python $pathGL_code/scripts/crab/readaMCatNloEntries.py 10 $sample_name.xml True
 done
 
