d85941799bc6ecab1150c3793c179cd2678edb42
Beren Ozek <bozek3@uic.edu>
2024-04-11 02:23:12 +0200

    edited for mttbar plots, final version

---
+++ plotter/combine_input.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python2
+
+import sys
+sys.argv.append('-b') # for root in batch mode
+import ROOT as root
+
+from collections import OrderedDict
+from plotter import NiceStackWithRatio, Process
+from constants import _YEARS
+from constants_combine_input import _REGIONS
+from constants_combine_input import _HISTNAME
+
+hist_name = _HISTNAME + '_'
+
+year = 'Run2'
+
+systematics = [
+    'pdf',
+    'mcscale',
+    'pu',
+    'prefiring',
+    'mu_id',
+    'mu_iso',
+    'mu_reco',
+    'mu_trigger',
+    'ele_id',
+    'ele_reco',
+    'ele_trigger',
+    'btag_cferr1',
+    'btag_cferr2',
+    'btag_hf',
+    'btag_hfstats1_UL16preVFP',
+    'btag_hfstats1_UL16postVFP',
+    'btag_hfstats1_UL17',
+    'btag_hfstats1_UL18',
+    'btag_hfstats2_UL16preVFP',
+    'btag_hfstats2_UL16postVFP',
+    'btag_hfstats2_UL17',
+    'btag_hfstats2_UL18',
+    'btag_lf',
+    'btag_lfstats1_UL16preVFP',
+    'btag_lfstats1_UL16postVFP',
+    'btag_lfstats1_UL17',
+    'btag_lfstats1_UL18',
+    'btag_lfstats2_UL16preVFP',
+    'btag_lfstats2_UL16postVFP',
+    'btag_lfstats2_UL17',
+    'btag_lfstats2_UL18',
+    'ttag_corr',
+    'ttag_uncorr_UL16preVFP',
+    'ttag_uncorr_UL16postVFP',
+    'ttag_uncorr_UL17',
+    'ttag_uncorr_UL18',
+    'tmistag',
+    'jec_UL16preVFP',
+    'jec_UL16postVFP',
+    'jec_UL17',
+    'jec_UL18',
+    'jer_UL16preVFP',
+    'jer_UL16postVFP',
+    'jer_UL17',
+    'jer_UL18',
+    'isr',
+    'fsr',
+]
+
+for key, var in _REGIONS.items():
+    region = var.get('name')
+
+    print("region: " + region)
+
+    # SM processes
+    processes_Plotter = [
+        Process(hist_name + region + '_TTToSemiLeptonic', 't#bar{t}', root.kRed, 1., 0.2),
+        Process(hist_name + region + '_TTToOthers', 't#bar{t}', root.kPink-3, 1., 0.2),
+        Process(hist_name + region + '_WJets', 'W+jets', root.kSpring-3, 1., 0.5),
+        Process(hist_name + region + '_ST', 'Single t', root.kBlue, 1., 0.7),
+        Process(hist_name + region + '_DY', 'Drell-Yan', root.kOrange, 1., 0.5),
+        Process(hist_name + region + '_Diboson', 'Diboson', root.kMagenta, 1., 0.5),
+        Process(hist_name + region + '_QCD', 'QCD', root.kAzure+10, 1., 0.5),
+        # Process(hist_name + region + '_others', 'Drell-Yan #plus Diboson #plus QCD', root.kOrange, 1., 0.5),
+        # Process(hist_name + region + '_others2', 'Drell-Yan #plus Diboson', root.kOrange, 1., 0.5),
+    ]
+    processes_Plotter_temp = OrderedDict()
+    for index, p in enumerate(processes_Plotter):
+        p.index = index
+        processes_Plotter_temp[p.name] = p
+        processes_Plotter = processes_Plotter_temp
+
+    # signal
+    signals_Plotter = [
+    
+        Process(hist_name + region + '_TTToSemiLeptonic', 't#bar{t} Semileptonic', root.kRed),
+        Process(hist_name + region + '_TTToOthers', 't#bar{t} Others', root.kPink-3, 1),
+    ]
+    signals_Plotter_temp = OrderedDict()
+    for index, s in enumerate(signals_Plotter):
+        s.index = index
+        signals_Plotter_temp[s.name] = s
+        signals_Plotter = signals_Plotter_temp
+
+
+    if region.startswith('SR'):
+        blinded = True
+        text_sim = 'Simulation'
+    else:
+        blinded = False
+        text_sim = ''
+
+    nice = NiceStackWithRatio(
+        infile_path = '/nfs/dust/cms/user/jabuschh/combine/CMSSW_11_3_4/src/HiggsAnalysis/CombinedLimit/B2G-22-006/01a_rootfiles/inputhists_run2_fa10000.root',
+        # infile_directory = '', # the directory within the ROOT file
+        x_axis_title = 'm(t#bar{t})',
+        x_axis_unit = 'GeV',
+        y_axis_min = 0.1,
+        y_axis_max = 1E+09,
+        prepostfit = 'prefitRaw',
+        processes = processes_Plotter.values(),
+        signals = signals_Plotter.values(),
+        syst_names = [], # systematics,
+        lumi_unc = _YEARS.get(year).get('lumi_unc'),
+        divide_by_bin_width = False,
+        data_name = hist_name + region + '_data_obs', # this is now real DATA! -> keep unblinded in SRs
+        text_prelim = 'Private Work',
+        text_simulation = text_sim,
+        text_top_left = year,
+        text_top_right = _YEARS.get(year).get('lumi_fb_display') + ' fb^{#minus1} (13 TeV)',
+        nostack = False,
+        logy = True,
+        blind_data = blinded,
+    )
+
+    nice.plot()
+    nice.canvas.cd()
+
+    # legend
+    leg_offset_x = 0.05
+    leg_offset_y = 0.23
+    legend = root.TLegend(nice.coord.graph_to_pad_x(0.45+leg_offset_x), nice.coord.graph_to_pad_y(0.45+leg_offset_y), nice.coord.graph_to_pad_x(0.7+leg_offset_x), nice.coord.graph_to_pad_y(0.73+leg_offset_y))
+    if not nice.blind_data: legend.AddEntry(nice.data_hist, 'Data', 'ep')
+    legend.AddEntry(nice.stack.GetStack().At(processes_Plotter[hist_name + region + '_TTbar'].index), processes_Plotter[hist_name + region + '_TTbar'].legend, 'f')
+    legend.AddEntry(nice.stack.GetStack().At(processes_Plotter[hist_name + region + '_WJets'].index), processes_Plotter[hist_name + region + '_WJets'].legend, 'f')
+    legend.AddEntry(nice.stack.GetStack().At(processes_Plotter[hist_name + region + '_ST'].index), processes_Plotter[hist_name + region + '_ST'].legend, 'f')
+    # legend.AddEntry(nice.stack.GetStack().At(processes_Plotter[hist_name + region + '_DY'].index), processes_Plotter[hist_name + region + '_DY'].legend, 'f')
+    # legend.AddEntry(nice.stack.GetStack().At(processes_Plotter[hist_name + region + '_Diboson'].index), processes_Plotter[hist_name + region + '_Diboson'].legend, 'f')
+    # legend.AddEntry(nice.stack.GetStack().At(processes_Plotter[hist_name + region + '_QCD'].index), processes_Plotter[hist_name + region + '_QCD'].legend, 'f')
+    legend.AddEntry(nice.stack.GetStack().At(processes_Plotter[hist_name + region + '_others'].index), processes_Plotter[hist_name + region + '_others'].legend, 'f')
+    # legend.AddEntry(nice.stack.GetStack().At(processes_Plotter[hist_name + region + '_others2'].index), processes_Plotter[hist_name + region + '_others2'].legend, 'f')
+    # legend.AddEntry(nice.signal_hists[signals_Plotter[hist_name + region + '_ALP_ttbar_signal'].index], signals_Plotter[hist_name + region + '_ALP_ttbar_signal'].legend, 'l')
+    # legend.AddEntry(nice.signal_hists[signals_Plotter[hist_name + region + '_ALP_ttbar_interference'].index], signals_Plotter[hist_name + region + '_ALP_ttbar_interference'].legend, 'l')
+    legend.AddEntry(nice.signal_hists[signals_Plotter[hist_name + region + '_SIB_negint_sqrtmu3'].index], signals_Plotter[hist_name + region + '_SIB_negint_sqrtmu3'].legend, 'l')
+    legend.AddEntry(nice.signal_hists[signals_Plotter[hist_name + region + '_SIB_posint_sqrtmu3'].index], signals_Plotter[hist_name + region + '_SIB_posint_sqrtmu3'].legend, 'l')
+    legend.SetTextSize(0.025)
+    legend.SetBorderSize(0)
+    legend.SetFillStyle(0)
+    legend.Draw()
+
+    # region tag
+    tlatex_region = root.TLatex(nice.coord.graph_to_pad_x(0.05), nice.coord.graph_to_pad_y(0.83), var.get('text'))
+    tlatex_region.SetTextAlign(13) # top left
+    tlatex_region.SetTextFont(42) # bold: 62
+    tlatex_region.SetTextSize(0.025)
+    tlatex_region.SetNDC()
+    tlatex_region.Draw()
+
+    nice.save_plot('04_combine_input/' + year + '/mtt_' + region + '.pdf')
+    nice.canvas.Close()
---
+++ plotter/combine_input_file_withflags.py
@@ -0,0 +1,289 @@
+from ROOT import *
+import os
+import sys
+import math
+from optparse import OptionParser
+      
+
+parser = OptionParser()
+parser.add_option("-y", "--year", dest="year", help="Specify the year (UL18, UL17, preUL16, postUL16)", type='str')
+parser.add_option("-l", "--lepton_flavor", dest="lepton_flavor", help="Specify the lepton flavor (electron, muon)", type='str')
+
+(options, args) = parser.parse_args()
+
+year = options.year if options.year else "UL18"  # default value if not specified
+lepton_flavor = options.lepton_flavor if options.lepton_flavor else "muon"  # default value if not specified
+
+
+# finalState = options.channel
+
+inputDir = "/nfs/dust/cms/user/beozek/uuh2-106X_v2/CMSSW_10_6_28/src/UHH2/ZprimeSemiLeptonic/output_DNN/{}/".format(year)
+combine_file_name = 'Mtt_{}_{}.root'.format(year, lepton_flavor)
+combine_file = TFile(combine_file_name, 'RECREATE')
+stackList = {"TTToSemiLeptonic", "TTToOthers", "WJets", "DY", "ST", "Diboson", "QCD", "DATA"}
+
+
+############ 
+# ----------------- other systematics except PDF, JER/JEC, murmuf ----------------
+############ 
+
+
+systematic_name_mapping = {
+    "mu_reco": "muonReco",
+    "pu": "pu",
+    "prefiring": "prefiringWeight",
+    "mu_id": "muonID",
+    "mu_iso": "muonIso",
+    "mu_trigger": "muonTrigger",
+    "ele_id" : "electronID", 
+    "ele_trigger": "electronTrigger",
+    "ele_reco": "electronReco",
+    "isr": "isr", 
+    "fsr": "fsr", 
+    "btag_cferr1": "btagCferr1", 
+    "btag_cferr2": "btagCferr2", 
+    "btag_hf": "btagHf",  
+    "btag_hfstats1": "btagHfstats1",
+    "btag_hfstats2": "btagHfstats2", 
+    "btag_lf": "btagLf", 
+    "btag_lfstats1": "btagLfstats1",
+    "btag_lfstats2": "btagLfstats2", 
+    "ttag_corr": "ttagCorr", 
+    "ttag_uncorr": "ttagUncorr", 
+    "tmistag": "tmistag"  
+    
+}
+
+# reads nominal histograms and computes projections for the "TTbar" sample. 
+# for other samples, it reads nominal histograms and writes them to the output file. 
+# It also processes and writes systematic variations, using systematic_name_mapping
+
+print(" ----------------- all systematics except PDF, JER/JEC, murmuf ----------------")
+for sample in stackList:
+    inFile = TFile.Open(inputDir + "{}/workdir_AnalysisDNN_{}_{}_Syst_rebin_v2/nominal/{}.root".format(lepton_flavor, year, lepton_flavor, sample), "READ")
+    if not inFile:
+        print("Input file for {} not found.".format(sample))
+        continue
+    
+    combine_file.cd()
+
+    
+    if sample == "DATA":
+        print("Processing sample for other sys: ", sample)
+        data_obs = inFile.Get("DNN_output0_General/M_Zprime_all").Clone("M_Zprime_DATA")
+        data_obs.Write("M_Zprime_DATA")
+    
+    else:
+        print("Processing sample for other sys: ", sample)
+        
+        h_nominal = inFile.Get("DeltaY_SystVariations_DNN_output0/M_Zprime").Clone("M_Zprime_" + sample)
+        h_nominal.Write("M_Zprime_" + sample)
+        
+        
+        for sys, new_sys_name in systematic_name_mapping.items():
+            print sys
+            
+            for variation in ["up", "down"]:
+                sys_hist_name = "M_Zprime_{}_{}".format(sys, variation)
+                sys_hist = inFile.Get("DeltaY_SystVariations_DNN_output0/" + sys_hist_name)
+                if sys_hist:
+                    output_hist_name = "M_Zprime_{}_{}{}".format(sample, sys, variation.capitalize())
+                    sys_hist.Clone(output_hist_name).Write()
+            
+         
+    inFile.Close()
+
+# processes each sample in v_samples to create envelope histograms for each variation in v_variations. 
+# histograms are normalized based on the first bin content and written to the DeltaY directory in the output ROOT file
+
+# calculates normalization factors, and then computes the maximum and minimum variations for each bin. The resulting histograms are named sample_murmufUp and sample_murmufDown
+
+############ 
+# ----------------- murmuf ----------------
+
+# 1) Retrieve histograms from the input file: nominal, upup, upnone
+# 2) Calculate normalization scales for each variation by dividing the first bin content of each variation histogram by the first bin content of the nominal histogram.
+# 3) Apply these normalization scales to the corresponding histograms from a vector of murmuf variations (like v_murmuf_upup, v_murmuf_upnone, etc.).
+# 4) For each set of variation histograms, find the maximum and minimum values in each bin among all variations, create two new histograms: hist_scaleUp and hist_scaleDown.
+# 5) Name these histograms depending on the specific convention
+############ 
+
+def getEnvelope(inputDir, v_samples, v_variations, combine_file):
+    
+    print(" ----------------- murmuf processing ----------------")
+    
+    for sample in v_samples:
+        inFile = TFile.Open(inputDir + "{}/workdir_AnalysisDNN_{}_{}_Syst_rebin_v2/nominal/{}.root".format(lepton_flavor, year, lepton_flavor, sample), "READ")       
+        if not inFile:
+            print("Input file for {} not found.".format(sample))
+            continue
+        combine_file.cd()
+
+        print("Processing sample for murmuf: ", sample)
+    
+        h_nominal = inFile.Get("DeltaY_SystVariations_DNN_output0/M_Zprime")
+        scales = {}
+        if not h_nominal:
+            print("Nominal histogram for {} not found.".format(sample))
+            continue
+
+        for variation in v_variations:
+            variation_hist = inFile.Get("DeltaY_SystVariations_DNN_output0/M_Zprime_murmuf_{}".format(variation))
+            
+            if variation_hist: 
+                if h_nominal.GetBinContent(1) != 0:
+                    scales[variation] = variation_hist.GetBinContent(1) / h_nominal.GetBinContent(1)
+            else:
+                print("Histogram for variation '{}' not found in {}".format(variation, sample))
+
+        hist_scaleUp = h_nominal.Clone("M_Zprime_" + sample + "_murmufUp")
+        hist_scaleDown = h_nominal.Clone("M_Zprime_" + sample + "_murmufDown")
+
+        # apply scales and find max/min for each bin
+        for bin_idx in range(1, h_nominal.GetNbinsX() + 1):
+            max_val = h_nominal.GetBinContent(bin_idx)
+            min_val = h_nominal.GetBinContent(bin_idx)
+            for var, scale in scales.items():
+                var_hist = inFile.Get("DeltaY_SystVariations_DNN_output0/M_Zprime_murmuf_{}".format(var))
+                if var_hist:
+                    scaled_val = var_hist.GetBinContent(bin_idx) / scale
+                    max_val = max(max_val, scaled_val)
+                    min_val = min(min_val, scaled_val)
+
+            hist_scaleUp.SetBinContent(bin_idx, max_val)
+            hist_scaleDown.SetBinContent(bin_idx, min_val)
+        
+        combine_file.cd()
+        hist_scaleUp.Write()
+        hist_scaleDown.Write()
+
+        inFile.Close()
+
+
+
+############ 
+# ----------------- PDFs (100) ----------------
+############ 
+
+
+def processPDF(inputDir, v_samples, combine_file):
+    print(" ----------------- PDFs processing ----------------")
+    
+    
+    for sample in v_samples:
+        inFile = TFile.Open(inputDir + "{}/workdir_AnalysisDNN_{}_{}_Syst_rebin_v2/nominal/{}.root".format(lepton_flavor, year, lepton_flavor, sample), "READ")
+        if not inFile:
+            print("Input file for {} not found.".format(sample))
+            continue
+        combine_file.cd()
+
+        if sample == "Diboson":
+                nominal_hist_name = "DeltaY_SystVariations_DNN_output0/M_Zprime"
+                nominal = inFile.Get(nominal_hist_name) 
+                if not nominal:
+                    print("Nominal histogram for Diboson not found.")
+                    continue
+                
+                hist_pdfUp = nominal.Clone("M_Zprime_Diboson_pdfUp")
+                hist_pdfDown = nominal.Clone("M_Zprime_Diboson_pdfDown")
+                hist_pdfUp.Write()
+                hist_pdfDown.Write()
+
+
+        else:
+            
+            print("Processing PDF: ", sample)
+            
+            # getting the nominal histogram
+            nominal = inFile.Get("DeltaY_SystVariations_DNN_output0/M_Zprime")
+            
+            if not nominal:
+                print("Nominal histogram for {} not found.".format(sample))
+                continue
+
+            # normalization scales for each PDF variation
+            v_pdf_norm = []
+            
+            # getting 100 PDFs histograms
+            for i in range(1, 101):
+                pdf_hist = inFile.Get("Zprime_PDFVariations_DNN_output0/M_Zprime_PDF_{}".format(i))
+                if pdf_hist:
+                    if nominal.GetBinContent(1) != 0:
+                        norm_scale_pdf = pdf_hist.GetBinContent(1) / nominal.GetBinContent(1)
+                        v_pdf_norm.append(norm_scale_pdf)
+                        pdf_hist.Scale(1. / norm_scale_pdf)
+
+            hist_pdfUp = TH1F("M_Zprime_{}_pdfUp".format(sample), "{} pdf up variation".format(sample), nominal.GetNbinsX(), nominal.GetXaxis().GetXmin(), nominal.GetXaxis().GetXmax())
+            hist_pdfDown = TH1F("M_Zprime_{}_pdfDown".format(sample), "{} pdf down variation".format(sample), nominal.GetNbinsX(), nominal.GetXaxis().GetXmin(), nominal.GetXaxis().GetXmax())
+
+            # Calculate RMS for each bin and update up/down histograms
+            for bin_idx in range(1, nominal.GetNbinsX() + 1):
+                sum_bins = 0.
+                for j in range(1, 101):
+                    pdf_hist = inFile.Get("Zprime_PDFVariations_DNN_output0/M_Zprime_PDF_{}".format(j))
+                    bin_content = pdf_hist.GetBinContent(bin_idx)
+                    sum_bins += (bin_content - nominal.GetBinContent(bin_idx)) ** 2
+
+                rms = math.sqrt(sum_bins / 100)
+                hist_pdfUp.SetBinContent(bin_idx, nominal.GetBinContent(bin_idx) + rms)
+                hist_pdfDown.SetBinContent(bin_idx, nominal.GetBinContent(bin_idx) - rms)
+
+            hist_pdfUp.Write()
+            hist_pdfDown.Write()
+
+        inFile.Close()
+            
+            
+# Diboson Sample:
+# For Diboson, I am cloning the nominal histogram for each of the 100 PDF variations and naming them Up or Down variation. So, this sample is without specific PDF variations.
+
+# TTbar Sample:
+# I am creating a matrix from four quadrants, calculating the projection for each, and then applying each of the 100 PDF variations.
+# For each PDF variation, I create a matrix, calculate its projections, and then calculate the deviation from the nominal projection for both projections (TTbar_1 and TTbar_2).
+
+# Other Samples:
+# For samples other than Diboson and TTbar, I am looping through each of the 100 PDF variations and calculate the Up and Down variations based on the deviation from the nominal histogram.
+
+############ 
+# ----------------- JER/JEC ----------------
+############ 
+
+
+def processJERJEC(inputDir, v_samples, combine_file, sys_variations):
+    print(" ----------------- JER/JEC processing ----------------")
+
+    for sys_variation in sys_variations: 
+        print(" --- passed to another JER/JEC variation ---")
+        for sample in v_samples:
+            sys_file = TFile.Open(inputDir + "{}/workdir_AnalysisDNN_{}_{}_{}_Syst/{}.root".format(lepton_flavor, year, lepton_flavor, sys_variation, sample), "READ")
+            if not sys_file:
+                print("Input file for {} variation {} not found.".format(sample, sys_variation))
+                continue
+            
+            combine_file.cd()
+
+            print("Processing {} for {} ".format(sample, sys_variation))
+            hist_name = "DNN_output0_General/M_Zprime"
+            jer_jec_hist = sys_file.Get(hist_name)
+            if jer_jec_hist:
+                jer_jec_hist.Clone("M_Zprime_{}_{}".format(sample, sys_variation.split('_')[0].lower() + sys_variation.split('_')[1].capitalize())).Write()
+            
+            else:
+                print("Missing histogram for {}: {}".format(sample, hist_name))
+                
+            sys_file.Close()
+
+
+
+sys_variations = ["JEC_up", "JEC_down", "JER_up", "JER_down"]
+
+v_samples = ["TTToSemiLeptonic", "TTToOthers", "WJets", "ST", "QCD", "DY", "Diboson"]
+v_variations = ["upup", "upnone", "noneup", "nonedown", "downnone", "downdown"]
+
+getEnvelope(inputDir, v_samples, v_variations, combine_file)
+
+processPDF(inputDir, v_samples, combine_file)
+
+processJERJEC(inputDir, v_samples, combine_file, sys_variations)
+
+combine_file.Close()
