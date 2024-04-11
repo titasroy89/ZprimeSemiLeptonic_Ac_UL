55925e46b3232596f93a2f339a9f445414606c7c
Beren Ozek <bozek3@uic.edu>
2024-04-11 02:21:54 +0200

    updated

--- analysis/Run_750_900.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm Ac_muon_750_900.root
-echo "removed old root file"
-
-text2workspace.py datacard.txt -o Ac_muon_750_900.root  -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel -m 125 --PO map='.*/TTbar_1:r_neg[1,0,20]' --PO map='.*/TTbar_2:r_pos=expr;;r_pos("53446/54239*@0*(100+@1)/(100-@1)",r_neg,Ac[-2,-5,0])' --PO verbose
